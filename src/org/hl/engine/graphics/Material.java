08928c65cb5c25d9ca4dacc310b79ae1f0c50e24
EvilMuffinHa <hkailadka88@gmail.com>
2020-05-08 22:23:06 -0400

    adding a TextureLoader without using SlickUtils

---
+++ src/org/hl/engine/graphics/Material.java
@@ -0,0 +1,44 @@
+package org.hl.engine.graphics;
+
+import org.hl.engine.utils.TextureLoader;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.openvr.Texture;
+
+import java.awt.image.BufferedImage;
+
+public class Material {
+
+    private Texture texture;
+
+    private BufferedImage image;
+
+    private int width, height;
+    private int textureID;
+
+    public Material(String path) {
+
+         this.image = TextureLoader.loadImage(path); //The path is inside the jar file
+
+    }
+    public void create() {
+        this.width = this.image.getWidth();
+        this.height = this.image.getHeight();
+        this.textureID = TextureLoader.loadTexture(image);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getTextureID() {
+        return textureID;
+    }
+
+    public void destroy() {
+        GL11.glDeleteTextures(textureID);
+    }
+}
